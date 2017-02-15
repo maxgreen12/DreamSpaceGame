a4d09c486a320896b7a5e7e07b5b3e353a2c540d
maxGreen <maxgreenuegene@gmail.com>
2017-02-15 10:34:23 -0800

    finished backgrounds. need to do dreamspace now

--- objects.js
+++ objects.js
@@ -49,11 +49,11 @@ var intros = [
     new intro('to be set',[2,2,0,0],["geuss I should go find 'em ","ugh. This means responsibility doesn't it...",'none','none']), //3
     new intro("you step outside into a courtyard. The bright sun makes your head pound.",[8,8,8,8], ["where am I?",'what time is it?',"I'm hungry",'none']), //4
     new intro("you reach into your pocket and fish out your phone. It's missing a screen.",[2,2,0,0],['why do bad things happen to good people.','to be set','none','none']), //5
-    new intro('blank',[2,2,2,2],['blank','none','none','none']),//6
-    new intro('blank',[2,2,2,2],['blank','none','none','none']),//7
-    new intro('blank',[2,2,2,2],['blank','none','none','none']),//8
-    new intro('blank',[2,2,2,2],['blank','none','none','none']),//9
-    new intro('blank',[2,2,2,2],['blank','none','none','none']), //10
+    new intro('you step through a crumbling stone door way into a room full of must. It smells like an old book. The walls are littered with pictographs',[2,2,2,2],['what is must?','mmmm old book smell.','why are there lights in here?','none']),//6
+    new intro('you see before you the exit n stuff',[2,2,2,2],['blank','none','none','none']),//7
+    new intro("you walk into a humid environment. before you stands a tree that looks like it's holding the weight of the world. This place is ancient.",[2,2,2,2],['as ancient as grandma?','hey look, a fern.','none','none']),//8
+    new intro("you feel like you just got put into tron. flashy lights and little beeps and boops fill the atmosphere. It's a lot to take in.",[2,2,2,2],['hey I have a quarter!',"what are these big boxes with screens for?",'none','none']),//9
+    new intro("you open a door and it feels like you stepped through a portal. it's forty degrees colder and you can't feel your legs, but the sight of this place is almost worth it.",[2,2,2,2],['this place got a thermostat?',"dang. wish I hadn't gambled away my coat.",'none','none']), //10
     new intro('blank',[2,2,2,2],['blank','none','none','none']) //11, this is the intro to the room you started in
 
 ];
@@ -62,11 +62,11 @@ var rooms = [
     //room(introId,description,[lf,f,rf,rb,b,lb],optionId,oldNew,img,mp3)
     new room(0,'there is an opening against the wall',['barred door',1,'barred door','n','n','n'],1,false,'https://s-media-cache-ak0.pinimg.com/originals/86/1b/da/861bda7d76e58f26f657789cf79893d8.jpg'), //0
     new room(4,'you find yourself in a courtyard that has six rooms surrounding it, and a nice fountain in the middle',[2,3,4,5,0,6],1,true,'https://upload.wikimedia.org/wikipedia/en/3/3a/Freer_Courtyard.jpg'), //1
-    new room(6,'egyptian temple',['n','n','n','barred door',1,'barred door'],0,true),
-    new room(7,'exit',['n','n','n','barred door',1,'barred door'],0,true),
-    new room(8,'jungle house',['n','n','n','barred door',1,'barred door'],0,true),
-    new room(9,'arcade',['n','n','n','barred door',1,'barred door'],0,true),
-    new room(10,'ice caves',['n','n','n','barred door',1,'barred door'],0,true)
+    new room(6,'egyptian temple',['n','n','n','barred door',1,'barred door'],0,true,'http://people.ucls.uchicago.edu/~bwildem/art_hist_laba/egypt/rockcut.png'),
+    new room(7,'exit',['n','n','n','barred door',1,'barred door'],0,true,'http://faculty.wartburg.edu/lindgrene/DSCN4645.JPG'),
+    new room(8,'jungle temple',['n','n','n','barred door',1,'barred door'],0,true,'https://davidlazarphoto.com/amp/wp-content/uploads/2012/07/18-David-Lazar-Jungle-Temple.jpg'),
+    new room(9,'arcade',['n','n','n','barred door',1,'barred door'],0,true,'http://www.roadtovr.com/wp-content/uploads/2016/08/new-retro-arcade-neon-launch-8.jpg'),
+    new room(10,'ice caves',['n','n','n','barred door',1,'barred door'],0,true,'http://www.glacierguides.is/sites/default/files/2000x1333_ice_caves_crystal_cave_gallery_2_einarolafurmatthiasson.jpg')
 ];
 
 var effects = [
