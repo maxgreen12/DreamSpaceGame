cae0049672e234c3dd1586cef52378334b4bbce9
maxGreen <maxgreenuegene@gmail.com>
2017-02-23 10:35:00 -0800

    finished dreamspace! just get new images and flavor text!

--- objects.js
+++ objects.js
@@ -57,14 +57,16 @@ var intros = [
     new intro("you walk into a humid environment. before you stands a tree that looks like it's holding the weight of the world. This place is ancient.",[2,2,2,2],['as ancient as grandma?','hey look, a fern.','none','none']),//8
     new intro("you feel like you just got put into tron. flashy lights and little beeps and boops fill the atmosphere. It's a lot to take in.",[2,2,2,2],['hey I have a quarter!',"what are these big boxes with screens for?",'none','none']),//9
     new intro("you open a door and it feels like you stepped through a portal. it's forty degrees colder and you can't feel your legs, but the sight of this place is almost worth it.",[2,2,2,2],['this place got a thermostat?',"dang. wish I hadn't gambled away my coat.",'none','none']), //10
-    new intro('blank',[2,2,2,2],['blank','none','none','none']), //11, this is the intro to the room you started in
+    new intro('you notice a little cot at the back of the room you came out of',[10,10,2,2],['well, one can also sleep on the ground...',"hmm, I didn't really consider sleeping.",'none','none']), //11, this is the intro to the room you started in
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //12
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //13
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //14
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //15
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //16
     new intro('blank',[2,2,2,2],['blank','none','none','none']), //17
-    new intro('blank',[2,2,2,2],['blank','none','none','none']) //18
+    new intro('blank',[2,2,2,2],['blank','none','none','none']), //18
+    new intro("you plop down on the bed. It's not that comfy but you sorta sink into it",[11,11,11,11],['wait who said that?','this place is insane',"I know! you couldn't dream it up",'yawn']), //19
+    new intro('the world shifts around you. It seems like you fell into a dream instead of falling asleep. The bed below you has changed and you realise your in a different room.',[2,2,2,2],['oh wow, this is nice','am I dreaming?','this feels so real','none']) //20
 
 ];
 
@@ -77,8 +79,8 @@ var rooms = [
     new room(8,'jungle temple',['n','n','n','barred door',1,'barred door'],1,true,11,'https://davidlazarphoto.com/amp/wp-content/uploads/2012/07/18-David-Lazar-Jungle-Temple.jpg'),
     new room(9,'arcade',['n','n','n','barred door',1,'barred door'],1,true,12,'http://www.roadtovr.com/wp-content/uploads/2016/08/new-retro-arcade-neon-launch-8.jpg'),
     new room(10,'ice caves',['n','n','n','barred door',1,'barred door'],1,true,13,'http://www.glacierguides.is/sites/default/files/2000x1333_ice_caves_crystal_cave_gallery_2_einarolafurmatthiasson.jpg'),
-    new room(12,'start mirror',['barred door',8,'barred door','n','n','n'],2,true,0,'https://s-media-cache-ak0.pinimg.com/originals/86/1b/da/861bda7d76e58f26f657789cf79893d8.jpg'),
-    new room(13,'courtyard mirror',[9,10,11,12,7,13],2,true,1,'https://upload.wikimedia.org/wikipedia/en/3/3a/Freer_Courtyard.jpg'),
+    new room(12,'start mirror',['barred door',8,'barred door','n','n','n'],2,true,0,'https://texasstation.sclv.com/~/media/Images/Page-Background-Images/Texas/TS_Hotel_King_lowrez.jpg?h=630&la=en&w=1080'),
+    new room(13,'courtyard mirror',[9,10,11,12,7,13],2,true,1,'http://grandinroad.scene7.com/is/image/frontgate/BLOG_fountain?defaultImage=NoImageIcon_GR&fmt=png&fit=crop,1&wid=600&hei=460&resMode=sharp2&op_usm=1.4,0.4,0,0'),
     new room(14,'egyptian temple mirror',['n','n','n','barred door',8,'barred door'],2,true,2,'http://people.ucls.uchicago.edu/~bwildem/art_hist_laba/egypt/rockcut.png'),
     new room(15,'exit mirror',['n','n','n','barred door',8,'barred door'],2,true,3,'http://faculty.wartburg.edu/lindgrene/DSCN4645.JPG'),
     new room(16,'jungle temple mirror',['n','n','n','barred door',8,'barred door'],2,true,4,'https://davidlazarphoto.com/amp/wp-content/uploads/2012/07/18-David-Lazar-Jungle-Temple.jpg'),
@@ -89,8 +91,9 @@ var rooms = [
 var effects = [
     //effect([functionId],buttonNames,oneUse)
     new effect([0,0,0,0],['none','none','none','none'],0),
-    new effect([6,7,0,0],['pull out your phone','go back to sleep','none','none'],0),
-    new effect([7,0,0,0],['wake up','none','none','none'],0)
+    new effect([6,7,0,0],['pull out your phone','none','none','none'],0),
+    new effect([7,0,0,0],['wake up','none','none','none'],0),
+    new effect([9,0,0,0],['go back to sleep','none','none','none'],0)
 ];
 
 var functions = [
@@ -154,9 +157,7 @@ var functions = [
             console.log("you're in a dream!");
             player.inDream = false;
             player.dreamLocation = player.location;
-            console.log(player.mirrorLocation);
-            console.log(findRoom().mirrorLocation);
-            if(player.mirrorLocation == findRoom().mirrorLocation){
+            if(player.mirrorLocation == findRoom().mirrorRoom){ //if you're in a parallel room
                 player.hasSoul = true;
                 console.log('you got your soul!');
             }
@@ -164,13 +165,13 @@ var functions = [
         }else { //if you're in the real world
             console.log("you're in reality!");
             player.mirrorLocation = player.location;
-            if(player.hasSoul){
+            if(player.hasSoul){ //if you have your soul
                 player.location = findRoom().mirrorRoom;
-            } else{
+            } else{ // if you don't have your soul
                 player.location = player.dreamLocation
             }
             player.inDream = true;
-            player.hasSoul = false
+            player.hasSoul = false;
         }
 
         $('.options').hide();
@@ -218,6 +219,28 @@ var functions = [
             }
         });
         console.log('8 HAPPEND')
+    },
+    function(){
+        effects[1].button[1] = 'go back to sleep';
+        rooms[0].options = 1;
+        effectHappens(19);
+        console.log('9 HAPPENED');
+    },
+    function(){
+        rooms[0].options = 3;
+        functions[2]();
+        console.log('10 HAPPENED');
+    },
+    function(){
+        player.location = 7;
+        var img = findRoom().img;
+        document.body.style.background = "url(" + img + ") ";
+        document.body.style.backgroundSize = 'cover';
+        findRoom().oldNew = false;
+        effectHappens(20);
+        player.inDream = true;
+        player.hasSoul = false;
+        console.log('11 HAPPENED');
     }
 ];
 // function(itemNameInQuotes,introNumber){
